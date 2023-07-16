16a1dd8d133622405b017eec2fb8392af9b70c77
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-17 02:26:45 +0500

    hms

---
+++ hms.dart
@@ -0,0 +1,10 @@
+void main (){
+  List<Map<String, dynamic>> patientData = [];
+  
+  print('***=== Hospital Management System ===***');
+}
+
+ patientId ({required String name, required int id, required int age}){
+
+ }
+
