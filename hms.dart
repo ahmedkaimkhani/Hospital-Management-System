aa7bafad3483507fe579523b1441e75e0cbf8b48
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-17 02:45:17 +0500

    hms

--- hms.dart
+++ hms.dart
@@ -4,11 +4,21 @@ void main (){
   List<Map<String, dynamic>> patientData = [];
   
   print('***=== Hospital Management System ===***');
-  stdout.write('Press 1: Create your Id: ');
-  String press1 = (stdin.readLineSync()!);
+  print('Create Your Id');
+  patientId(name: '', id: 0, age: 0);
 }
 
  patientId ({required String name, required int id, required int age}){
 
+  String nameInput = '';
+  int idInput = 0;
+  int ageInput = 0;
+
+  stdout.write('Enter your name: $nameInput');
+  nameInput = (stdin.readLineSync()!);
+  stdout.write('Enter your name: $idInput');
+  idInput = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter your name: $ageInput');
+  ageInput = int.parse(stdin.readLineSync()!);
  }
 
