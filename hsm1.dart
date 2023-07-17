70b1d2e0a774299d967cf6902f441791a146b0ab
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-18 02:24:41 +0500

    hsm

--- hsm1.dart
+++ hsm1.dart
@@ -1,1 +1,28 @@
-void main (){}
+
+import 'dart:io';
+
+List<Map<String, dynamic>> patientList = [];
+void main (){
+
+}
+
+pageOne (){
+  bool condition = true;
+
+  while(condition == true){
+    print('');
+    print('***=== Hospital Management System ===***');
+    print('');
+    print('Press 1: Add Patient');
+    print('Press 2: View Patients Records');
+    print('Press 3: Search Patients');
+    print('Press 4: Appoinments Schedule');
+    print('Press 5: View Appoinments Schedule');
+    print('Press 6: Exit Program');
+
+    print('');
+    stdout.write('Enter you option: ');
+    int option = int.parse(stdin.readLineSync()!);
+    print('');
+  }
+}
